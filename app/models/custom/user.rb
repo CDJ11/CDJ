494722645ae68aea920ac88f7eb496c5e6a5cd39
clairezed <clairezuliani@gmail.com>
2018-04-27 16:29:41 +0200

    #32 Fix some tests broken because of user changes

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -9,12 +9,36 @@ class User < ActiveRecord::Base
             :lastname,
             :date_of_birth,
             :postal_code,
-            presence: true
+            presence: true, if: :username_required?
 
   private def set_default_username
     self.username = [lastname, firstname].join(" ")
   end
-  before_validation :set_default_username, if: -> { username.blank? }
+  before_validation :set_default_username, if: -> { username.blank? && !erased? }
+
+  # Instance methods ==================================================
+
+  def erase(erase_reason = nil)
+    update(
+      erased_at: Time.current,
+      erase_reason: erase_reason,
+      username: nil,
+      email: nil,
+      unconfirmed_email: nil,
+      phone_number: nil,
+      encrypted_password: "",
+      confirmation_token: nil,
+      reset_password_token: nil,
+      email_verification_token: nil,
+      confirmed_phone: nil,
+      unconfirmed_phone: nil,
+      firstname: nil,
+      lastname: nil,
+      postal_code: nil,
+      date_of_birth: nil
+    )
+    identities.destroy_all
+  end
 
 
 end
