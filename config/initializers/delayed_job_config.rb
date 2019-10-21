c74168a28db94b2a24325dd95f63e13025cc0b8d
clairezed <clairezuliani@gmail.com>
2019-10-21 10:17:36 +0200

    Add delayed_job start on boot script

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -11,3 +11,28 @@ Delayed::Worker.read_ahead = 10
 Delayed::Worker.default_queue_name = 'default'
 Delayed::Worker.raise_signal_exceptions = :term
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+
+
+# Custom script to launch delayed job on startup, if server is to reboot
+# cf https://stackoverflow.com/questions/2580871/start-or-ensure-that-delayed-job-runs-when-an-application-server-restarts
+DELAYED_JOB_PID_PATH = "#{Rails.root}/tmp/pids/delayed_job.pid"
+
+def start_delayed_job
+  Thread.new do
+    `ruby script/delayed_job start`
+  end
+end
+
+def process_is_dead?
+  begin
+    pid = File.read(DELAYED_JOB_PID_PATH).strip
+    Process.kill(0, pid.to_i)
+    false
+  rescue
+    true
+  end
+end
+
+if !File.exist?(DELAYED_JOB_PID_PATH) && process_is_dead?
+  start_delayed_job
+end
